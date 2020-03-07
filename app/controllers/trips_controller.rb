d5ea4e32402769ea38c690fbcf8a3665b86c7188
Nick Semerad <nicksemerad@gmail.com>
2020-03-07 11:42:05 -0700

    fixed trip controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,7 +1,5 @@
-
-
 class TripsController < ApplicationController
-  
+  before_action :trip_params
   before_action :set_trip, only: [:show, :edit, :update, :destroy]
   
   def index
@@ -9,18 +7,26 @@ class TripsController < ApplicationController
   end
 
   def show
-
   end
 
   def new
     @trip = Trip.new
   end
 
+  def create
+    @trip = Trip.new(trip_params)
+    if @trip.save
+      redirect_to @trip
+    else
+      render :new
+    end
+  end
+
   def edit
   end
 
   def update 
-    if trip.update(trip_params) 
+    if @trip.update(trip_params) 
       redirect_to @trip
     else
       render :edit 
@@ -28,24 +34,19 @@ class TripsController < ApplicationController
   end
 
   def destroy 
-    Trip.find(params[:id]).destroy
+    @trip.destroy
     redirect_to root_path
   end 
-
-  
 
   private 
 
   def set_trip
-    trip = Trip.find(params[:id])
-
+    @trip = Trip.find(params[:id])
   end
   
   def trip_params
-    params.require(:trip) .permit(:name) 
+    params.require(:trip).permit(:name) 
   end
-
-
 
 end
 
